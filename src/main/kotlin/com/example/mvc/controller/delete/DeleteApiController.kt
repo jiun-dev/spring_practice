90b9c435654bd6558f2b8ffdb7304eabf502567b
shinjiun <shinjiun@shinjiunui-MacBookPro.local>
2021-05-09 13:38:53 +0900

    validation

--- src/main/kotlin/com/example/mvc/controller/delete/DeleteApiController.kt
+++ src/main/kotlin/com/example/mvc/controller/delete/DeleteApiController.kt
@@ -1,22 +1,29 @@
 package com.example.mvc.controller.delete
 
+import org.jetbrains.annotations.NotNull
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.constraints.Min
 
 @RestController
 @RequestMapping("/api")
+@Validated // kotlin에서 java bin 유효성을 검증하기 위해
 class DeleteApiController {
 
-    @DeleteMapping("/delete-mapping")
+    @DeleteMapping(path = ["/delete-mapping"])
     fun deleteMapping(
-        @RequestParam name: String,
-        @RequestParam age: Int,
-    ): String {
-        println(name)
-        println(age)
+        @RequestParam(value = "name") _name: String,
 
-        return name + ' ' + age
+        @javax.validation.constraints.NotNull(message = "age값이 누락되었습니다.")
+        @Min(value = 20, message = "20보다 커야됩니다.")
+        @RequestParam _age: Int,
+    ): String {
+        println(_name)
+        println(_age)
+
+        return _name + " " + _age
     }
 }
