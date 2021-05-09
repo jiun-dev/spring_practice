55f9ada53e338301e188fefe632dabe4f29247dd
shinjiun <shinjiun@shinjiunui-MacBookPro.local>
2021-05-09 14:22:47 +0900

    원하는 validation이 없을때 커스텀

--- src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
+++ src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
@@ -3,6 +3,9 @@ package com.example.mvc.model.http
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.databind.annotation.JsonNaming
+import java.lang.Exception
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import javax.validation.constraints.*
 
 // @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy::class) // ->case by case 자동으로 스네이크케이스로 받아진다.
@@ -23,4 +26,17 @@ class UserRequest(
     // @JsonProperty(value = "phone_number") -> 일일이 설정하려면 힘들다!
     @field:Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}\$") // 정규식
     var phoneNumber:String? = null,
-    )
+
+    var createdAt:String? = null, // yyyy-MM-dd HH:mm:ss
+    ){
+    @AssertTrue(message = "생성일자의 패턴은 yyyy-MM-dd HH:mm:ss")
+    private fun isValidCreatedAt():Boolean{
+
+        return try {
+            LocalDateTime.parse(this.createdAt, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+            true
+        }catch (e:Exception){
+            false
+        }
+    }
+}
