7735c4f394bff097f68a3930bc2ee6a9e9cefa32
shinjiun <shinjiun@shinjiunui-MacBookPro.local>
2021-05-09 14:14:17 +0900

    error msg

--- src/main/kotlin/com/example/mvc/controller/put/PutApiController.kt
+++ src/main/kotlin/com/example/mvc/controller/put/PutApiController.kt
@@ -3,10 +3,14 @@ package com.example.mvc.controller.put
 import com.example.mvc.model.http.Result
 import com.example.mvc.model.http.UserRequest
 import com.example.mvc.model.http.UserResponse
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.BindingResult
+import org.springframework.validation.FieldError
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/api")
@@ -18,33 +22,44 @@ class PutApiController {
     }
 
     @PutMapping("/put-mapping/object")
-    fun putMappingObject(@RequestBody userRequest: UserRequest): UserResponse {
+    fun putMappingObject(@Valid @RequestBody userRequest: UserRequest, bindingResult : BindingResult): ResponseEntity<String> {
 
-        return UserResponse().apply {
-            this.result = Result().apply {
-                this.resultCode = "OK"
-                this.resultMessage = "성공"
+        if(bindingResult.hasErrors()){
+            val msg = StringBuilder()
+            bindingResult.allErrors.forEach{
+                val field = it as FieldError
+                val message = it.defaultMessage
+                msg.append(field.field+ " : " + message + "\n")
             }
-        }.apply {
-            this.description = "~~~~~~"
-        }.apply {
-            val userList = mutableListOf<UserRequest>()
-            userList.add(userRequest)
-            userList.add(UserRequest().apply {
-                this.name = "a"
-                this.age = 10
-                this.address = "b"
-                this.email = "c"
-                this.phoneNumber = "11111111111"
-            })
-
-            userList.add(UserRequest().apply {
-                this.name = "b"
-                this.age = 20
-                this.address = "b.address"
-                this.email = "c.email"
-                this.phoneNumber = "11111112222"
-            })
+            return ResponseEntity.badRequest().body(msg.toString())
         }
+        return ResponseEntity.ok("")
+
+//        return UserResponse().apply {
+//            this.result = Result().apply {
+//                this.resultCode = "OK"
+//                this.resultMessage = "성공"
+//            }
+//        }.apply {
+//            this.description = "~~~~~~"
+//        }.apply {
+//            val userList = mutableListOf<UserRequest>()
+//            userList.add(userRequest)
+//            userList.add(UserRequest().apply {
+//                this.name = "a"
+//                this.age = 10
+//                this.address = "b"
+//                this.email = "c"
+//                this.phoneNumber = "11111111111"
+//            })
+//
+//            userList.add(UserRequest().apply {
+//                this.name = "b"
+//                this.age = 20
+//                this.address = "b.address"
+//                this.email = "c.email"
+//                this.phoneNumber = "11111112222"
+//            })
+//        }
     }
 }
--- src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
+++ src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
@@ -3,14 +3,24 @@ package com.example.mvc.model.http
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.databind.annotation.JsonNaming
+import javax.validation.constraints.*
 
 // @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy::class) // ->case by case 자동으로 스네이크케이스로 받아진다.
 class UserRequest(
+
+    @field:NotEmpty
+    @field:Size(min = 2, max = 8) // 길이
     var name: String? = null,
+
+    @field:PositiveOrZero // 0 <=  양수를 검증
     var age: Int? = null,
+    @field:NotBlank // 공백 X
     var address: String? = null,
+
+    @field:Email // email양식 검증
     var email: String? = null,
 
     // @JsonProperty(value = "phone_number") -> 일일이 설정하려면 힘들다!
+    @field:Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}\$") // 정규식
     var phoneNumber:String? = null,
     )
